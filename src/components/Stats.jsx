a9eab35cd48bb0986cc116f1328c977a6b3df064
Nikhil Magar <nikhilmagar1995@gmail.com>
2023-04-22 15:41:31 +0530

    Changes Done As per Masai Requirements

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -25,8 +25,8 @@ export const Stats=()=>{
                 }}
                  href="https://github.com/Nikras512">
                     <Image id="github-streak-stats"
-                    // https://github-readme-streak-stats.herokuapp.com?user=Nikras512&theme=shades-of-purple&fire=orangered&ring=orange&currStreakNum=8860D0&sideNums=8860D0&sideLabels=8860D0
-                    src="https://streak-stats.demolab.com/?user=Nikras512&theme=shades-of-purple&fire=orangered&ring=orange&currStreakNum=8860D0&sideNums=8860D0&sideLabels=8860D0" 
+                    src="https://github-readme-streak-stats.herokuapp.com?user=Nikras512&theme=shades-of-purple&fire=orangered&ring=orange&currStreakNum=8860D0&sideNums=8860D0&sideLabels=8860D0&bg_color=8860D0"
+                    // src="https://streak-stats.demolab.com/?user=Nikras512&theme=shades-of-purple&fire=orangered&ring=orange&currStreakNum=8860D0&sideNums=8860D0&sideLabels=8860D0" 
                     alt="Stat-1"
                     width="100%"
                     align="center"
@@ -55,8 +55,8 @@ export const Stats=()=>{
                          href="https://github.com/Nikras512"
                         >
                         <img id="github-stats-card"
-                        // https://github-readme-stats.vercel.app/api?username=Nikras512
-                        src="https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/?username=nikras512&show_icons=true&theme=shades-of-purple&ring=orange&title=purple" 
+                        src="https://github-readme-stats.vercel.app/api?username=Nikras512&theme=shades-of-purple"
+                        // src="https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/?username=nikras512&show_icons=true&theme=shades-of-purple&ring=orange&title=purple" 
                         alt="Stat-2" />
                         </a>
                         <a 
@@ -71,9 +71,9 @@ export const Stats=()=>{
                         marginTop:"3%"
                     }} 
                     id="github-top-langs"
-                    // https://github-readme-stats.vercel.app/api/top-langs/?username=Nikras512
+                    src="https://github-readme-stats.vercel.app/api/top-langs/?username=Nikras512&theme=shades-of-purple"
                     // https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/top-langs/?username=nikras512
-                        src="https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/top-langs/?username=nikras512&theme=shades-of-purple" 
+                        // src="https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/top-langs/?username=nikras512&theme=shades-of-purple" 
                         alt="Stat-3" />
                         </a>
                     </Box>
